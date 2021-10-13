c1ab83586caf21557281c7f79efbfff386a3170f
erick-cubillo <67172915+erick-cubillo@users.noreply.github.com>
2021-10-13 16:48:13 -0600

    Add files via upload

---
+++ coef_aire.m
@@ -0,0 +1,12 @@
+function [ajuste_foto , ajuste_compt] = coef_aire(e) 
+
+ajuste_foto = 2*10^(-06) * (e/1000)^(-3.155);
+if e<=20
+ajuste_compt = 5e-06 * (e/1000)^(-2.963);
+else
+    ajuste_compt= 7.113*10^-6*(e/1000)^(-2.821)+0.1439
+end
+
+
+
+end
