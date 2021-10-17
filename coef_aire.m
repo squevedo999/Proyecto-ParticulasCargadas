40b9e2e6ce02f382fc35b88d8ae44a64a7d5aa87
erick-cubillo <67172915+erick-cubillo@users.noreply.github.com>
2021-10-16 21:13:41 -0600

    Add files via upload

--- coef_aire.m
+++ coef_aire.m
@@ -1,11 +1,11 @@
 function output = coef_aire(e) 
 
-    ajuste_foto = 2*10^(-06) * (e/1000)^(-3.155);
+    ajuste_foto = 2*10^(-06) * (e/1000)^(-3.103);
 
     if e<=20
         ajuste_compt = 5e-06 * (e/1000)^(-2.963);
     else
-        ajuste_compt= 7.113*10^-6*(e/1000)^(-2.821)+0.1439;
+        ajuste_compt= 0.07024*(e/1000)^(-0.3125)+exp(e*-83.39);
     end
     
     output = [ajuste_foto , ajuste_compt];
